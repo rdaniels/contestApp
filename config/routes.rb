4fc7de5bbc03af9ca8514fe202acce29e4e63608
Rob Daniels <rob@danielscorporation.com>
2014-12-25 07:00:28 -0500

    shopify integration broken

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   root 'dashboard#index'
 
+  resources :accounts do
+    member do
+      get 'test_connection'
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
